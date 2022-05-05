19386a4e7dfb84f87bf46bacd6985ddb8a2416b8
Antoine Loriette <antoine.loriette@gmail.com>
2022-05-05 18:44:32 +0200

    new data file

--- viz/utils.py
+++ viz/utils.py
@@ -30,27 +30,30 @@ def format_data(df):
 
         row = eval(row['data'].replace('false', 'False'))
 
-        key = row['sample_key']
-        t0 = row['timestamp0']
-        t0_norm = 0
-        ts = row['timestamp']
-        stroke_id = row['stroke_id']
-        segment_id = row['segment_id']
+        try:
+            key = row['sample_key']
+            t0 = row['timestamp0']
+            t0_norm = 0
+            ts = row['timestamp']
+            stroke_id = row['stroke_id']
+            segment_id = row['segment_id']
 
-        x, y, p = row.get('xyp', default_value)
-        x_, y_, p_ = row.get('rel_xyp', default_value)
-        x0, y0, p0 = row.get('rel_xyp_lp', default_value)
-        x1, y1, p1 = row.get('xyp_sg', default_value)
-        s = row['s']
-        angle = row['angle']
-        da = row['da']
-        da = da[0] if isinstance(da, (list,)) else da
-    
-        new_row = [key, t0, t0_norm, ts, stroke_id, segment_id, 
-                   x, y, p, x_, y_, p_, x0, y0, p0, x1, y1, p1,
-                   s, angle, da]
+            x, y, p = row.get('xyp', default_value)
+            x_, y_, p_ = row.get('rel_xyp', default_value)
+            x0, y0, p0 = row.get('rel_xyp_lp', default_value)
+            x1, y1, p1 = row.get('xyp_sg', default_value)
+            s = row['s']
+            angle = row['angle']
+            da = row['da']
+            da = da[0] if isinstance(da, (list,)) else da
 
-        new_rows.append(new_row)
+            new_row = [key, t0, t0_norm, ts, stroke_id, segment_id, 
+                       x, y, p, x_, y_, p_, x0, y0, p0, x1, y1, p1,
+                       s, angle, da]
+
+            new_rows.append(new_row)
+        except KeyError:
+            print("KeyError", i, row)
 
     data = pd.DataFrame(data=new_rows,
                         columns=['key', 't0', 't0_norm', 'ts',
