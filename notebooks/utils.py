7a67fe6aa11c253db148c71727303284fb650a55
Diemo Schwarz <schwarz@ircam.fr>
2022-08-31 12:13:50 +0200

    factorise read_log, start analysing P2 novelty in new notebook

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -2,6 +2,7 @@
 # pandas select
 from functools import reduce
 from operator import and_, or_
+
 def select(df, **kwargs):
     '''Builds a boolean array where columns indicated by keys in kwargs are tested for equality to their values.
     In the case where a value is a list, a logical or is performed between the list of resulting boolean arrays.
@@ -29,3 +30,32 @@ def select(df, **kwargs):
         res = df
 
     return res
+
+def read_log(logfile):
+
+    data = []
+    segment = []
+    event = 'False'
+
+    with open(logfile) as f:
+        for i, line in enumerate(f):
+            line = eval(line.replace('null', 'False'))
+
+            if line['logtype'] == 'event':
+                if line['event'] == ['Start Condition 1']:
+                    event = 'c1'
+                if line['event'] == ['Start Condition 2']:
+                    event = 'c2'
+                if line['event'] == ['Familiarization']:
+                    event = 'c0'
+                
+                print(i, line)
+
+            if line['logtype'] == 'segment':
+                line['condition'] = event
+                segment.append(line)
+            if line['logtype'] == 'data':
+                line['condition'] = event
+                data.append(line)
+
+    return data, segment
